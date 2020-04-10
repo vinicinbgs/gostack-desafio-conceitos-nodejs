c49ed86aae5970b18e449afe1531b3a3faa9e1cf
Vinicius Dutra <dutra_morais@hotmail.com>
2020-04-10 17:34:58 -0300

    all tests passed

--- src/app.js
+++ src/app.js
@@ -1,33 +1,81 @@
+const { uuid } = require("uuidv4");
+
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+const Middleware = (request, response, next) => {
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id === id
+  );
+
+  if (findRepositoryIndex === -1) {
+    return response.status(400).json({ error: 'Repository doesnt not exists' })
+  }
+
+  request.params.findRepositoryIndex = findRepositoryIndex;
+
+  next();
+};
+
+app.put("/repositories/:id", Middleware, (request, response) => {
+  const { findRepositoryIndex, id } = request.params;
+  const { title, url, techs } = request.body;
+  console.log(id);
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+  repositories[findRepositoryIndex] = repository;
+
+  return response.json(repository)
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", Middleware, (request, response) => {
+  const { findRepositoryIndex } = request.params;
+
+  repositories.splice(findRepositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", Middleware, (request, response) => {
+  const { findRepositoryIndex } = request.params;
+
+  repositories[findRepositoryIndex].likes += 1;
+
+  return response.json(repositories[findRepositoryIndex]);
 });
 
 module.exports = app;
